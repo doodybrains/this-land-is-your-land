131faeac111c2f4d14d8e3f9c9eff97a8663aadb
doodybrains <doodybrainz@gmail.com>
2018-12-11 23:16:26 -0500

    Update timing

--- sketch.js
+++ sketch.js
@@ -42,118 +42,125 @@ function draw(){
       textFont("Fira Sans Light");
       text('to Fira Sans Light', y, 320);
     }
-    if (sound.currentTime() > 13.67 && sound.currentTime() < 16.31) {
+    if (sound.currentTime() > 13.67 && sound.currentTime() < 16.22) {
       textFont("Playfair Display");
       text('from Playfair Display', y, 320);
     }
-    if (sound.currentTime() > 16.31 && sound.currentTime() < 19.71) {
+    if (sound.currentTime() > 16.22 && sound.currentTime() < 19.74) {
       textFont("Ubuntu");
       text('to Ubuntu Regular', y, 320);
     }
-    if (sound.currentTime() > 19.71 && sound.currentTime() < 23.89) {
+    if (sound.currentTime() > 19.74 && sound.currentTime() < 23.81) {
       textFont("Bungee");
       text('this type was made for you and me', y, 320);
     }
     //////////////////////
-    if (sound.currentTime() > 23.89 && sound.currentTime() < 26.35) {
+    if (sound.currentTime() > 23.81 && sound.currentTime() < 26.33) {
       textSize(80);
       can.style('letter-spacing', '1.5em');
       textFont("Black And White Picture");
-      text('As I was typing', y, 320);
+      text('As I was typing', y*1.5, 320);
     }
-    if (sound.currentTime() > 26.35 && sound.currentTime() < 29.04) {
+    if (sound.currentTime() > 26.33 && sound.currentTime() < 29.24) {
       textSize(84);
       can.style('letter-spacing', '1.25em');
       textFont("Incosolata");
-      text('this series of texts,', y, 320);
+      text('this series of texts', y*1.5, 320);
     }
-    if (sound.currentTime() > 29.04 && sound.currentTime() < 31.53) {
+    if (sound.currentTime() > 29.24 && sound.currentTime() < 31.32) {
       textSize(86);
       can.style('letter-spacing', '1.75em');
       textFont("Open Sans Condensed");
-      text('I kept on looking', y, 320);
+      text('I kept on looking', y*1.5, 320);
     }
-    if (sound.currentTime() > 31.53 && sound.currentTime() < 34.26) {
+    if (sound.currentTime() > 31.32 && sound.currentTime() < 34.11) {
       textSize(88);
       can.style('letter-spacing', '1.35em');
       textFont("Indie Flower");
-      text('at the fonts on Google', y, 320);
+      text('at the fonts on Google', y*1.5, 320);
     }
-    if (sound.currentTime() > 34.26 && sound.currentTime() < 36.65) {
+    if (sound.currentTime() > 34.11 && sound.currentTime() < 36.56) {
       textSize(90);
       can.style('letter-spacing', '1.65em');
       textFont("Playfair Display");
-      text('I wished I knew of', y, 320);
+      text('I wished I knew of', y*1.5, 320);
     }
-    if (sound.currentTime() > 36.65 && sound.currentTime() < 39.92) {
-      textSize(92);
+    if (sound.currentTime() > 36.56 && sound.currentTime() < 39.47) {
+      textSize(99);
       can.style('letter-spacing', '1.15em');
       textFont("Gamja Flower");
-      text('a better place but', y, 320);
+      text('a better website', y*1.5, 320);
+
     }
-    if (sound.currentTime() > 39.92 && sound.currentTime() < 44.1) {
+    if (sound.currentTime() > 39.47  && sound.currentTime() < 40.06) {
+      textSize(120);
+      can.style('letter-spacing', '1.65em');
+      textFont("Playfair Display");
+      text('but', y*1.5, 320);
+    }
+    if (sound.currentTime() > 40.06 && sound.currentTime() < 44.1) {
       textSize(60);
       can.style('letter-spacing', '0');
       textFont("Merriweather");
-      text('this type was made for you and me.', y, 320);
+      text('this type was made for you and me', y*1.5, 320);
     }
     //////////////////////
     if (sound.currentTime() > 44.1 && sound.currentTime() < 46.58) {
       textFont("Bungee");
-      text('so I followed my searches,', y, 320);
+      text('I followed my searches,', y, 320);
     }
     if (sound.currentTime() > 46.58 && sound.currentTime() < 49.42) {
       textFont("Incosolata");
-      text('to a site called Typewolf,', y, 320);
+      text('to a site called Typewolf', y, 320);
     }
     if (sound.currentTime() > 49.42 && sound.currentTime() < 51.53) {
       textFont("Open Sans Condensed");
       text('they have a P D F', y, 320);
-    } 
+    }
     if (sound.currentTime() > 51.53 && sound.currentTime() < 54.26) {
-      textFont("Indie Flower");
+      textFont("Merriweather");
       text('called the Guide To Free Fonts', y, 320);
     }
     if (sound.currentTime() > 54.26 && sound.currentTime() < 56.65) {
-      textFont("Playfair Display");
+      textFont("Bungee");
       text('you have to buy it...', y, 320);
     }
     if (sound.currentTime() > 56.65 && sound.currentTime() < 58.42) {
-      textFont("Gamja Flower");
+      textFont("Playfair Display");
       text('it costs thirty nine dollars', y, 320);
     }
     if (sound.currentTime() > 59.42 && sound.currentTime() < 64.1) {
-      textFont("Merriweather");
-      text('this type was made for you and me.', y, 320);
+      textFont("Gamja Flower");
+      text('this type was made for you and me', y, 320);
     }
     /////////////////
     if (sound.currentTime() > 64.1 && sound.currentTime() < 66.58) {
       textFont("Nunito Sans");
-      text('The font Futura,', y, 320);
+      text('The font Futura,', y * 0.5, 320);
     }
     if (sound.currentTime() > 66.58 && sound.currentTime() < 69.42) {
       textFont("Nunito Sans");
-      text('made in nineteen twenty seven,', y, 320);
+      text('made in nineteen twenty seven,', y * 0.5, 320);
     }
     if (sound.currentTime() > 69.42 && sound.currentTime() < 71.53) {
       textFont("Nunito Sans");
-      text('was meant to carry us', y, 320);
+      text('was meant to carry us', y * 0.5, 320);
     } 
     if (sound.currentTime() > 71.53 && sound.currentTime() < 74.26) {
       textFont("Nunito Sans");
-      text('to a utopian society', y, 320);
+      text('to a utopian society', y * 0.5, 320);
     }
     if (sound.currentTime() > 74.26 && sound.currentTime() < 76.65) {
       textFont("Nunito Sans");
-      text('its used in logos,', y, 320);
+      text('its used in logos', y * 0.5, 320);
     }
-    if (sound.currentTime() > 76.65 && sound.currentTime() < 80.42) {
+    if (sound.currentTime() > 76.65 && sound.currentTime() < 80.25) {
       textFont("Nunito Sans");
-      text(`like Domino's Pizza`, y, 320);
+      text(`like Domino's Pizza`, y * 0.5, 320);
     }
-    if (sound.currentTime() > 80.42 && sound.currentTime() < 85.1) {
+    if (sound.currentTime() > 80.25 && sound.currentTime() < 85.1) {
       textFont("Nunito Sans");
-      text('this free version was made for you and me', y, 320);
+      text('this free version was made for you and me', y * 0.5, 320);
     }
   }
 }
